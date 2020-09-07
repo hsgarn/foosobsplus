6f5d0ca914c8ccb98232f4f21756027719b0dadf
Hugh Garner <register@hsgarn.com>
2020-09-07 00:11:53 -0500

    v1.048: Start Match button now clears Match Winner and Meatball fields

--- src/com/midsouthfoosball/foosobsplus/model/Match.java
+++ src/com/midsouthfoosball/foosobsplus/model/Match.java
@@ -78,6 +78,8 @@ public class Match implements Serializable {
 		clearScores();
 		clearTimes();
 		clearGameWinners();
+		clearMatchWinner();
+		clearMeatball();
 		startGame();
 	}
 	public void startGame() {
