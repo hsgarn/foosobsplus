cf488fd42024aab5f3951b47f443028c9ad74e66
hsgarn <48559736+hsgarn@users.noreply.github.com>
2025-12-24 16:32:01 -0600

    v2.063 12/24/2025
    Add Maven Wrapper

--- README.md
+++ README.md
@@ -1022,6 +1022,9 @@ As you can see by the revision history below, I have spent many hours working on
 [![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/donate/?business=MQLATTDXA7CPJ&no_recurring=0&currency_code=USD)
 
 ## Revision History</br>
+v2.063 12/24/2025</br>
+Add Maven Wrapper</br>
+</br>
 v2.062 11/29/2025</br>
 Add Ball Panel and supporting logic.</br>
 </br>
--- src/main/java/com/midsouthfoosball/foosobsplus/view/AboutPanel.java
+++ src/main/java/com/midsouthfoosball/foosobsplus/view/AboutPanel.java
@@ -42,8 +42,8 @@ import net.miginfocom.swing.MigLayout;
 
 public class AboutPanel extends JPanel {
 	private static final long serialVersionUID 	= 1L;
-	private static final String VERSIONNUMBER 	= "2.062"; //$NON-NLS-1$
-	private static final String RELEASEDATE 	= "11/29/2025"; //$NON-NLS-1$
+	private static final String VERSIONNUMBER 	= "2.063"; //$NON-NLS-1$
+	private static final String RELEASEDATE 	= "12/24/2025"; //$NON-NLS-1$
 	private static final String AUTHOR          = "Hugh Garner"; //$NON-NLS-1$
 	private static final String COPYRIGHT 		= "2020-2026 Hugh Garner"; //$NON-NLS-1$
 	public AboutPanel(JFrame aboutFrame) {
